ab506e3db6771517da5aa74b05b2830917290d21
shakk1l1 <thibault.fesler@gmail.com>
2023-04-17 12:48:25 +0200

    gesture detector

--- app/src/main/java/com/example/ba2projinfo/test classes/MainActivity.kt
+++ app/src/main/java/com/example/ba2projinfo/test classes/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.ba2projinfo
 
 //import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 //import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Math.abs
 
 class MainActivity: AppCompatActivity() {
 
@@ -13,6 +17,7 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         canonView = findViewById<CanonView>(R.id.vMain)
+        gestureDetector = GestureDetector(this)
     }
 
     override fun onPause() {
@@ -23,5 +28,63 @@ class MainActivity: AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         canonView.resume()
+    }
+
+    private lateinit var gestureDetector: GestureDetector
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
+
+    // Override this method to recognize touch event
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    // All the below methods are GestureDetector.OnGestureListener members
+    // Except onFling, all must "return false" if Boolean return type
+    // and "return" if no return type
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+        return
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        return
+    }
+
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            val diffY = e2.y - e1.y
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > swipeThreshold && abs(velocityX) > swipeVelocityThreshold) {  // C'est ici qu'on va pouvoir voir quelle distance il a bougé son doigt
+                    if (diffX > 0) {
+                        Toast.makeText(applicationContext, "Left to Right swipe gesture", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(applicationContext, "Right to Left swipe gesture", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return true
     }
 }
