9927d6754f8376e4bb3eb8e049152bd88a472ef4
MTheMighty <martinanderlecht@gmail.com>
2023-05-06 19:02:44 +0200

    arrêtez de tout casser le code

--- app/src/main/java/com/example/angryducks/LevelView.kt
+++ app/src/main/java/com/example/angryducks/LevelView.kt
@@ -88,7 +88,7 @@ class LevelView @JvmOverloads constructor (context: Context, attributes: Attribu
         }
     private val pigs = arrayOf(pig1, pig2, pig3, pig4)
     private val birds = arrayOf(bird1, bird2, bird3, bird4, bird5, bird6, bird7)
-    private val objets = arrayOf(pig1, pig2, pig3, pig4,bird1, bird2, bird3, bird4, bird5, bird6, bird7)
+    private val objets = arrayOf(bird1, bird2, bird3, bird4, bird5, bird6, bird7,pig1, pig2, pig3, pig4)
     private val blocs = arrayOf(bloc1)
     private var gameOver = false
     private var totalElapsedTime = 0.0
