e901dced088a22307f87c37c6ca0c6bc3f592b52
MTheMighty <martinanderlecht@gmail.com>
2023-05-10 00:52:20 +0200

    COMMENTS

--- app/src/main/java/com/example/angryducks/ObstacleRectangle.kt
+++ app/src/main/java/com/example/angryducks/ObstacleRectangle.kt
@@ -19,9 +19,9 @@ class ObstacleRectangle(
     // Variables init
     //----------------------------------------------------------------------------------------------
     val hpinit = hp
-    private val nx= sin(angle)
+    private val nx= sin(angle)  //Calcul du vecteur normal à l'obstacle
     private val ny= cos(angle)
-    private val segmentup = ObstacleSegment(positionx+nx*largeur/2,positiony+ny*largeur/2,longueur,nx,ny)
+    private val segmentup = ObstacleSegment(positionx+nx*largeur/2,positiony+ny*largeur/2,longueur,nx,ny)       //Initialisation des objets qui composent l'obstacle rectangle
     private val segmentdown=ObstacleSegment(positionx-nx*largeur/2,positiony-ny*largeur/2,longueur,-nx,-ny)
     private val segmentleft=ObstacleSegment (positionx+ny*(longueur/2),positiony-nx*longueur/2,largeur,ny,-nx)
     private val segmentright=ObstacleSegment (positionx-ny*(longueur/2),positiony+nx*longueur/2,largeur,-ny,nx)
@@ -30,8 +30,8 @@ class ObstacleRectangle(
     private val pointdownright=ObstaclePoint(positionx-nx*(largeur/2-7)+ny*(longueur/2-7),positiony-ny*(largeur/2-7)-nx*(longueur/2-7), 3.0)
     private val pointdownleft=ObstaclePoint(positionx-nx*(largeur/2-7)-ny*(longueur/2-7),positiony-ny*(largeur/2-7)+nx*(longueur/2-7), 3.0)
     private var obstaclePaint = Paint()
-    val obstaacles= arrayOf(segmentup,segmentleft,segmentdown,segmentright)
-    val pooints= arrayOf(pointupleft,pointupright,pointdownleft,pointdownright)
+    val obstaacles= arrayOf(segmentup,segmentleft,segmentdown,segmentright)         //Liste des composantes point
+    val pooints= arrayOf(pointupleft,pointupright,pointdownleft,pointdownright)     //Liste des composantes segment
 
     //----------------------------------------------------------------------------------------------
     // Function
@@ -41,8 +41,8 @@ class ObstacleRectangle(
         obstaclePaint.strokeWidth = largeur.toFloat()
         obstaclePaint.color= Color.BLACK
     }
-    override fun set_posy(newy: Float){
-        positiony += newy
+    override fun set_posy(newy: Float){         //Entrées: newy, Sorties:None
+        positiony += newy                       //Modifie la position des compasante de l'obstacle pour d'adapter à la taille de l'écran
         for (obstacle in obstaacles){
             obstacle.postiony +=newy
         }
@@ -51,13 +51,13 @@ class ObstacleRectangle(
         }
     }
 
-    fun reset(){
-        killed = false
+    fun reset(){                //Entrées:None ,Sorties:None
+        killed = false          //Réinitialise l'obstacle
         obstaclePaint.color = Color.BLACK
         hp = hpinit
     }
-    fun draw(canvas: Canvas){
-        if(!killed) {
+    fun draw(canvas: Canvas){       //Entrées:canva ,Sorties:None
+        if(!killed) {               //Affiche l'obstacle à l'écran
             canvas.drawLine(
                 (positionx + ny * (longueur/ 2)).toFloat(),
                 (positiony - nx * (longueur/ 2)).toFloat(),
@@ -68,13 +68,13 @@ class ObstacleRectangle(
         }
     }
 
-    fun getkilled():Boolean {return killed}
+    fun getkilled():Boolean {return killed}     //Entrées:None ,Sorties:retourne si l'obstacle à été détruit
 
-    override fun low() {
-        obstaclePaint.color = Color.parseColor("#aaaaaa")
+    override fun low() {                                            //Entrées:None ,Sorties:None
+        obstaclePaint.color = Color.parseColor("#aaaaaa")   //Modifie la couleur de l'objet lorsque celui-ci est endommagé
     }
 
-    override fun mid() {
-        obstaclePaint.color = Color.parseColor("#808080")
+    override fun mid() {                                            //Entrées:None ,Sorties:None
+        obstaclePaint.color = Color.parseColor("#808080")  //Modifie la couleur de l'objet lorsque celui-ci est endommagé
     }
 }
