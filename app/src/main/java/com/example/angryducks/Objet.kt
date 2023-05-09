0ff7f0f692ce750c345854d0e0feb494d565011a
MTheMighty <martinanderlecht@gmail.com>
2023-05-10 00:40:51 +0200

    COMMENTS

--- app/src/main/java/com/example/angryducks/Objet.kt
+++ app/src/main/java/com/example/angryducks/Objet.kt
@@ -26,7 +26,7 @@ abstract class Objet(
     // Function
     //----------------------------------------------------------------------------------------------
 
-    fun getonscreen(): Boolean {return onscreen}
+    fun getonscreen(): Boolean {return onscreen}    //Entrées:None , Sorties:retourne si l'objet est présent sur la zone de l'écran
     fun getradius(): Float{return radius}
 
     open fun reset(){   //Entrées:None , Sorties:None
@@ -62,17 +62,17 @@ abstract class Objet(
         }
     }
 
-    fun collisionSphereSphere(x1:Double,y1:Double,r1:Double,x2:Double,y2:Double,r2:Double) {
-        val one = ((x1-x2).pow(2)+(y1-y2).pow(2)).pow(0.5)
+    fun collisionSphereSphere(x1:Double,y1:Double,r1:Double,x2:Double,y2:Double,r2:Double) {    //Entrées:position x, y de l'objet 1 et 2 et leurs rayons , Sorties:None
+        val one = ((x1-x2).pow(2)+(y1-y2).pow(2)).pow(0.5)                             //Détecte les collisions entre les objets en calculant leur distance et en la comparant à leur rayon
         val two = r1+r2
         colliding =(one<two)
 
     }
     fun sphereCollideSphere(v1x:Double,v1y:Double,m1:Double,v2x:Double,v2y:Double,m2:Double,coef:Double, objet: Objet) {
         val vmoyx:Double = (m1*v1x+m2*v2x)/(m1+m2)
-        val vmoyy:Double = (m1*v1y+m2*v2y)/(m1+m2)
-        val dv1x=(1.0+coef)*(v1x-vmoyx)
-        val dv1y=(1.0+coef)*(v1y-vmoyy)
+        val vmoyy:Double = (m1*v1y+m2*v2y)/(m1+m2)  //Entrées: vitesse x et y des 2 objets ainsi que leurs masses respectives, Sorties:None
+        val dv1x=(1.0+coef)*(v1x-vmoyx)             //Calcule les vitesses finale des 2 objets qui se collisionnent, modifie la vitesse de l'objet exécutat la méthode
+        val dv1y=(1.0+coef)*(v1y-vmoyy)             //et appelle le deuxième objet pour qu'il change lui même sa vitesse
         val dv2x=(1+coef)*(v2x-vmoyx)
         val dv2y=(1+coef)*(v2y-vmoyy)
         vitessex-=dv1x
@@ -82,8 +82,8 @@ abstract class Objet(
         objet.changeaftercoll(dv2x, dv2y)
     }
 
-    open fun changeaftercoll(v2x:Double, v2y:Double) {
-        vitessex-=v2x
+    open fun changeaftercoll(v2x:Double, v2y:Double) {      //Entrées:différence de vitesse x et y , Sorties:None
+        vitessex-=v2x                                       //Change la vitesse de l'objet collisioné avec le résultat calculé dans la fonction juste au dessus
         vitessey-=v2y
         collidingObjectCountDown=10
     }
@@ -94,15 +94,15 @@ abstract class Objet(
     }
 
 
-    open fun collideground(){
-        val prodvect=vitessex * Collision.nx-vitessey*Collision.ny
-        if (-50.0<prodvect && prodvect<=0.0) {
+    open fun collideground(){                    //Entrées:None , Sorties:None
+        val prodvect=vitessex * Collision.nx-vitessey*Collision.ny  //Calcule le changement de vitesse que subit un objet après avoir touché le sol
+        if (-50.0<prodvect && prodvect<=0.0) {  //Contact permanent // et change la vitesse de l'objet
             val dvx : Double = prodvect * Collision.nx
             val dvy : Double = -prodvect * Collision.ny
             vitessex = (vitessex - dvx)*(1.0-Collision.coefRoulement)
             vitessey = (vitessey - dvy)*(1.0-Collision.coefRoulement)
         }
-        else if (prodvect<=-50.0){
+        else if (prodvect<=-50.0){  //Choc
             val dvx : Double = prodvect * Collision.nx * (1+Collision.absorbtion)
             val dvy : Double = -prodvect * Collision.ny * (1+Collision.absorbtion)
             vitessex = (vitessex - dvx)
@@ -115,20 +115,20 @@ abstract class Objet(
 
         return if (((positionx - coo.x) * nx + (positiony - coo.y) * ny).absoluteValue < radius){
             ((positionx - coo.x) * ny - (positiony - coo.y) * nx).absoluteValue < ( longueur/2)
-        } else{
-            false
+        } else{              //Entrées:position en x et y du segment de l'obstacle, sa longueur et son orientation (nx,ny), le vecteur normal
+            false            // Sorties:retourne si l'objet concerné touche le sol du niveau
         }
     }
-    fun collideobstaclesegment(nx:Double, ny:Double, bloc: ObstacleRectangle){
-        val prodvect=vitessex * nx+vitessey*ny
-        if (-50.0<prodvect && prodvect< 0.0) {
+    fun collideobstaclesegment(nx:Double, ny:Double, bloc: ObstacleRectangle){      //Entrées:vecteur normal au segment (nx,ny) , Sorties:None
+        val prodvect=vitessex * nx+vitessey*ny                                      //Calcule le changement de vitesse que subit un objet après avoir touché un segment d'un obstacle
+        if (-50.0<prodvect && prodvect< 0.0) {//Contact permanent                   //et modifie la vitesse de l'objet concerné
             val dvx : Double = prodvect * nx
             val dvy : Double = prodvect * ny
             vitessex = (vitessex - dvx)*(1.0-Collision.coefRoulement)
             vitessey = (vitessey - dvy)*(1.0-Collision.coefRoulement)
 
         }
-        else if (-50.0>=prodvect){
+        else if (-50.0>=prodvect){              //Choc
             val dvx: Double = prodvect * nx * (1 + Collision.absorbtion)
             val dvy: Double = prodvect * ny * (1 + Collision.absorbtion)
             bloc.deteriorationdetect(vitessex, vitessey, mass, bloc.hpinit)
@@ -138,22 +138,22 @@ abstract class Objet(
         }
     }
 
-    fun touchingobstaclepoint(positionx:Double,positiony:Double,rayon:Double) :Boolean{
-        return((coo.x-positionx).pow(2)+(coo.y-positiony).pow(2) < (radius+rayon).pow(2))
+    fun touchingobstaclepoint(positionx:Double,positiony:Double,rayon:Double) :Boolean{             //Entrées:la position du point de collision en x et y et son rayon
+        return((coo.x-positionx).pow(2)+(coo.y-positiony).pow(2) < (radius+rayon).pow(2))  //Sortie: Retourne si l'objet rentre en collision avec le point
     }
 
-    fun collideobstaclepoint(positionx:Double,positiony:Double, bloc: ObstacleRectangle){
-        val distance=((coo.x-positionx).pow(2)+(coo.y-positiony).pow(2)).pow(0.5)
-        val nx=(coo.x-positionx)/distance
+    fun collideobstaclepoint(positionx:Double,positiony:Double, bloc: ObstacleRectangle){   //Entrées:La position du point de collision et le bloc auquel il appartient
+        val distance=((coo.x-positionx).pow(2)+(coo.y-positiony).pow(2)).pow(0.5)  //Sorties: None
+        val nx=(coo.x-positionx)/distance                           //Calcule la collision entre l'objet et le point de collision et modifie la vitesse de l'objet après calcul
         val ny=(coo.y-positiony)/distance
         val prodvect=vitessex * nx+vitessey*ny
-        if (0.0<=prodvect && prodvect<50.0) {
+        if (0.0<=prodvect && prodvect<50.0) {//Contact permanent
             val dvx : Double = prodvect * nx
             val dvy : Double = prodvect * ny
             vitessex = (vitessex - dvx)
             vitessey = (vitessey - dvy)
         }
-        else if (50.0>=prodvect) {
+        else if (50.0>=prodvect) {//Choc
             val dvx : Double = prodvect * nx * (1+Collision.absorbtion)
             val dvy : Double = prodvect * ny * (1+Collision.absorbtion)
             bloc.deteriorationdetect(vitessex, vitessey, mass,bloc.hpinit)
